d0acd252ff740902193444d7453797f7b624aad4
Tiffany Meng <ttmeng@unc.edu>
2024-09-09 09:53:02 -0400

    Finished practice with booleans

--- lessons/booleans.py
+++ lessons/booleans.py
@@ -1,6 +1,7 @@
 """Practice with booleans"""
 
 def check_first_letter(word:str, letter:str) -> str:
+    """Checks if letter is first character of word"""
     if word[0] == letter:
         return "match!"
     else:
