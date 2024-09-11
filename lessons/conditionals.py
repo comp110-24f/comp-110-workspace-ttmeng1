445726f1363f2151a81b88bcb5cffb1ee6f74763
Tiffany Meng <ttmeng@unc.edu>
2024-09-11 14:19:19 -0400

    Finished CQ02!

--- lessons/conditionals.py
+++ lessons/conditionals.py
@@ -2,10 +2,9 @@
 
 
 def get_weather_report() -> str:
+    """Takes type of weather as input and returns advice"""
     weather: str = input("What is the weather?")
-    if weather == "rainy":
-        print("Bring a jacket!")
-    elif weather == "cold":
+    if weather == ("rainy" or "cold"):
         print("Bring a jacket!")
     elif weather == "hot":
         print("Keep cool out there!")
